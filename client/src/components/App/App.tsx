4baf60abbcf039b897a139c5b0a2e34e90c21d58
DrusMarkonii <andrey8566ok@gmail.com>
2022-01-12 17:11:41 +0300

    feat: add navbar

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Navbar from '../navbar/Navbar';
 import './App.scss';
 
-const App: React.FC = (): any => {
-    <Navbar />;
-    <div>hi</div>;
-};
+const App: React.FC = () => <Navbar />;
 
 export default App;
--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 
 const Navbar: React.FC = () => (
-    <nav>
-        <div className='nav-wrapper blue darken-1'>
-            <a href='/' className='brand-logo'>
-                DruSNet
-            </a>
-            <ul className='right hide-on-med-and-down'>
-                <li>
-                    <a href='/'>Profile</a>
-                </li>
-                <li>
-                    <a href='/'>Friends</a>
-                </li>
-                <li>
-                    <a href='/'>Dialogs</a>
-                </li>
-                <li>
-                    <a href='/'>News</a>
-                </li>
-                <li>
-                    <a href='/'>Exit</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  <nav>
+    <div className='nav-wrapper blue darken-1 px1'>
+      <a href='/' className='brand-logo'>
+        AruSNet
+      </a>
+      <ul className='right hide-on-med-and-down'>
+        <li>
+          <a href='/'>Profile</a>
+        </li>
+        <li>
+          <a href='/'>Friends</a>
+        </li>
+        <li>
+          <a href='/'>Dialogs</a>
+        </li>
+        <li>
+          <a href='/'>News</a>
+        </li>
+        <li>
+          <a href='/'>Exit</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
