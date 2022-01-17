bf3a039b58ca6ecd1ffb4622de0822a09723e3c4
DrusMarkonii <andrey8566ok@gmail.com>
2022-01-17 12:15:04 +0300

    feat: add routing

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
-// import Profile from '../Profile/Profile';
+import Profile from '../Profile/Profile';
 import Messages from '../Messages/Messages';
 import './App.css';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <Header />
     <div className='container'>
       <Navbar />
-      {/* <Profile /> */}
-      <Messages />
+      <Routes>
+        <Route path='/Profile' element={<Profile />} />
+        <Route path='/Messages' element={<Messages />} />
+      </Routes>
     </div>
     <Footer />
-  </>
+  </Router>
 );
 
 export default App;
