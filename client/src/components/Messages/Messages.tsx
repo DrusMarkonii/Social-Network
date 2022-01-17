e6764969e3ff889398b9bf0a3f987e9d10137c37
DrusMarkonii <andrey8566ok@gmail.com>
2022-01-17 16:58:24 +0300

    feat: add render used map

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -3,18 +3,32 @@ import './Messages.css';
 import Dialog from './Dialog/Dialog';
 import DialogMassage from './Dialog/DialogMassage';
 
+const dialogData = [
+  { id: 1, username: 'Drus' },
+  { id: 2, username: 'Rob' },
+  { id: 3, username: 'Bob' },
+  { id: 4, username: 'Jon' },
+];
+
+const messageData = [
+  { id: 1, message: 'Ok. buy!' },
+  { id: 2, message: 'What are you doing?' },
+  { id: 3, message: 'how are you?' },
+  { id: 4, message: 'Hello' },
+];
+
+const DialogsItems = dialogData.map((item) => (
+  <Dialog username={item.username} id={item.id} key={item.id} />
+));
+
+const MessagesItems = messageData.map((item) => (
+  <DialogMassage message={item.message} key={item.id} />
+));
+
 const Messages: React.FC = () => (
   <div className='messages'>
-    <div className='dialogBox'>
-      <Dialog username='Drus' id={1} />
-      <Dialog username='Rob' id={2} />
-      <Dialog username='Bob' id={3} />
-    </div>
-    <div className='messagesBox'>
-      <DialogMassage message='Ok. buy!'/>
-      <DialogMassage message='how are you?'/>
-      <DialogMassage message='Hello'/>
-    </div>
+    <div className='dialogBox'>{DialogsItems}</div>
+    <div className='messagesBox'>{MessagesItems}</div>
   </div>
 );
 
--- client/src/components/Profile/MyPosts/MyPosts.tsx
+++ client/src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 import './MyPosts.css';
 import Post from './Post/Post';
 
+const PostData = [
+  {
+    id: 1,
+    username: 'Drus Markoni',
+    message: 'Welcome to AruSNet with Drus',
+    likesCount: 5,
+  },
+  {
+    id: 2,
+    username: 'Jane Pokr',
+    message: 'Welcome to AruSNet with Jane',
+    likesCount: 3,
+  },
+  {
+    id: 3,
+    username: 'Romale',
+    message: 'Welcome to AruSNet with Romale',
+    likesCount: 1,
+  },
+];
+
+const PostItems = PostData.map((item) => (
+  <Post
+    username={item.username}
+    message={item.message}
+    likesCount={item.likesCount}
+    key={item.id}
+  />
+));
+
 const MyPosts = () => (
   <div className='myPosts'>
     <div>
@@ -11,8 +41,7 @@ const MyPosts = () => (
         <button type='submit'>Add Post</button>
         <button type='submit'>Remove</button>
       </div>
-      <Post username='Drus Markoni' message='Welcome to AruSNet with Drus' likesCount={0} />
-      <Post username='Jane' message='Welcome to AruSNet with Jane' likesCount={5}/>
+      {PostItems}
     </div>
   </div>
 );
