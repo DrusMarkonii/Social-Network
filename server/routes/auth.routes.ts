8a9986454e5d4a2fa4962bcda2f6423665ee40ac
DrusMarkonii <andrey8566ok@gmail.com>
2022-01-19 14:11:37 +0300

    feat: add login routes

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import User from '../models/User/User';
 import bcrypt from 'bcrypt';
+import  config from 'config';
+import jwt from 'jsonwebtoken';
 import { check, validationResult } from 'express-validator';
+
 
 const router = Router();
 
@@ -15,7 +18,6 @@ router.post(
   // ],
   async ( req, res ) => {
     try {
-      console.log(req.body);
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ message: 'Incorrect request', errors });
@@ -39,10 +41,47 @@ router.post(
       return res.json({ message: `User ${userName} was created` });
 
     } catch (e) {
-      console.log('Errors Router', e);
+      console.log('Errors Router Registration', e);
       res.send({ message: 'Server error' });
     }
   }
 );
+
+
+router.post('/login', 
+async ( req, res ) => {
+  try {
+    const { email, password} = req.body;
+    const user = await User.findOne({email})
+
+    if(!user) {
+      return res.status(400).json({message: 'User not found'})
+    }
+
+    const isPassValid = bcrypt.compareSync(password, user.password)
+
+    if(!isPassValid) {
+      return res.status(400).json({message: 'Invalid password'})
+    }
+    
+    const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'})
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        avatar: user.avatar,
+        dialogs: user.dialogs,
+        posts: user.posts,
+        friends: user.friends
+      }
+    })
+
+  } catch (e) {
+    console.log('Errors Router Login', e);
+    res.send({ message: 'Server error' });
+  }
+}
+)
 
 export default router;
