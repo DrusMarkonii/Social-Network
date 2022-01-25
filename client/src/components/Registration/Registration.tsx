f63cbd19efeb9072aaaae852e7440cec9d8440f4
DrusMarkonii <andrey8566ok@gmail.com>
2022-01-25 16:14:41 +0300

    feat: added creaate users on client

--- client/src/components/Registration/Registration.tsx
+++ client/src/components/Registration/Registration.tsx
@@ -1,41 +1,47 @@
 import React, { useState } from 'react';
 import './Registration.css';
+import axios from 'axios';
 
 const Registration: React.FC = () => {
-  const [inputValue, setValue] = useState({
-    userName: '',
-    password: '',
-    email: ''
-  })
+ 
+
+  const [userName, setUserName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
 
-  const handelChange = (e:any) => {
-    const {name, value} = e.target;
-    setValue({
-      ...value,
-      [name]: value
 
-    })
-  }
   
-  const submitHandler = () => {
-    console.log('submit');
-    console.log(inputValue)
+  const submitHandler = async () => {
+    if (userName && email && password !== '') {
+      await axios.post('http://localhost:5000/api/auth/registration', {
+        userName,
+        email,
+        password
+      });
+    } else {
+      alert('Enter text!');
+    }
+
+    setUserName('')
+    setEmail('')
+    setPassword('')
   };
+
 
   return (
     <div className='registration'>
       <div>
         User Name
-        <input type='text' placeholder='User name' name='userName' onChange={handelChange}/>
+        <input type='text' placeholder='User name' value={userName} onChange={(e:any)=> setUserName(e.target.value)}/>
       </div>
       <div>
         Email
-        <input type='text' placeholder='Email' name='email'/>
+        <input type='text' placeholder='Email' value={email} onChange={(e:any)=> setEmail(e.target.value)}/>
       </div>
       <div>
         Password
-        <input type='text' placeholder='Password' name='password' />
+        <input type='text' placeholder='Password' value={password} onChange={(e:any)=> setPassword(e.target.value)}/>
       </div>
       <div>
         <input onClick={submitHandler} type='submit' value='Register' />
